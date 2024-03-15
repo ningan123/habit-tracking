22fe387bec538c7a0ecab85ea24c7cc09bd9202a
anwy13 <anwy13@chinaunicom.cn>
2024-03-15 11:14:59 +0800

    change date detail, add month

--- pkg/date/date.go
+++ pkg/date/date.go
@@ -6,22 +6,24 @@ import (
 )
 
 // TODO：2024-12-30的输出结果不太对
-func GetDateDetails(inputDate string) (int, int, time.Weekday, error) {
+func GetDateDetails(inputDate string) (int, time.Month, int, time.Weekday, error) {
 	// 将输入的日期解析为time.Time类型
 	date, err := time.Parse("2006-01-02", inputDate)
 	if err != nil {
-		return 0, 0, 0, fmt.Errorf("无效的日期格式: %s", err)
+		return 0, 0, 0, 0, fmt.Errorf("无效的日期格式: %s", err)
 	}
 
-	// 获取年份、周数和星期几
+	// 获取年份、月份、周数和星期几
 	year := date.Year()
-	week := date.Weekday()
+	month := date.Month()
 	_, weekNumber := date.ISOWeek()
+	weekday := date.Weekday()
+	
 
 	// 如果日期在第一周，但是年份不同，需要修正周数
 	if weekNumber == 1 && date.Month() == 12 {
 		year++
 	}
 
-	return year, weekNumber, week, nil
+	return year, month, weekNumber, weekday, nil
 }
--- pkg/date/date_test.go
+++ pkg/date/date_test.go
@@ -11,6 +11,7 @@ func TestGetDateDetails(t *testing.T) {
 	testCases := []struct {
 		inputDate        string
 		expectedYear     int
+		expectedMonth    time.Month
 		expectedWeekNum  int
 		expectedWeekday  time.Weekday
 		expectedErrorMsg string
@@ -51,29 +52,28 @@ func TestGetDateDetails(t *testing.T) {
 		// {"2023-12-30", 2023, 52, time.Saturday, ""},  // 周六
 		// {"2023-12-31", 2023, 52, time.Sunday, ""},    // 周日
 		// 2024年年初8天
-		{"2024-01-01", 2024, 1, time.Monday, ""},     // 周一
-		{"2024-01-02", 2024, 1, time.Tuesday, ""},
-		{"2024-01-03", 2024, 1, time.Wednesday, ""},
-		{"2024-01-04", 2024, 1, time.Thursday, ""},
-		{"2024-01-05", 2024, 1, time.Friday, ""},
-		{"2024-01-06", 2024, 1, time.Saturday, ""},
-		{"2024-01-07", 2024, 1, time.Sunday, ""},
-		{"2024-01-08", 2024, 2, time.Monday, ""},	
+		{"2024-01-01", 2024, 1, 1, time.Monday, ""},     // 周一
+		{"2024-01-02", 2024, 1, 1, time.Tuesday, ""},
+		{"2024-01-03", 2024, 1, 1, time.Wednesday, ""},
+		{"2024-01-04", 2024, 1, 1, time.Thursday, ""},
+		{"2024-01-05", 2024, 1, 1, time.Friday, ""},
+		{"2024-01-06", 2024, 1, 1, time.Saturday, ""},
+		{"2024-01-07", 2024, 1, 1, time.Sunday, ""},
+		{"2024-01-08", 2024, 1, 2, time.Monday, ""},	
 		// 2024年年末8天
-		{"2024-12-25", 2024, 52, time.Wednesday, ""},
-		{"2024-12-26", 2024, 52, time.Thursday, ""},
-		{"2024-12-27", 2024, 52, time.Friday, ""},
-		{"2024-12-28", 2024, 52, time.Saturday, ""},
-		{"2024-12-29", 2024, 52, time.Sunday, ""},
+		{"2024-12-25", 2024, 12, 52, time.Wednesday, ""},
+		{"2024-12-26", 2024, 12, 52, time.Thursday, ""},
+		{"2024-12-27", 2024, 12, 52, time.Friday, ""},
+		{"2024-12-28", 2024, 12, 52, time.Saturday, ""},
+		{"2024-12-29", 2024, 12, 52, time.Sunday, ""},
 		// {"2024-12-30", 2024, 53, time.Monday, ""},
 		// {"2024-12-31", 2024, 53, time.Tuesday, ""},
 
-		{"2024-01-01", 2024, 1, time.Monday, ""},     // 周一
 	}
 
 	// 遍历测试用例表
 	for _, tc := range testCases {
-		year, weekNum, weekday, err := GetDateDetails(tc.inputDate)
+		year, month, weekNum, weekday, err := GetDateDetails(tc.inputDate)
 
 		// 检查错误情况
 		if tc.expectedErrorMsg != "" {
@@ -89,7 +89,7 @@ func TestGetDateDetails(t *testing.T) {
 		}
 
 		// 检查结果
-		if year != tc.expectedYear || weekNum != tc.expectedWeekNum || weekday != tc.expectedWeekday {
+		if year != tc.expectedYear || month != tc.expectedMonth || weekNum != tc.expectedWeekNum || weekday != tc.expectedWeekday {
 			t.Errorf("对于输入日期 %s，期望的结果为 年份：%d，周数：%d，星期几：%s，实际结果为 年份：%d，周数：%d，星期几：%s",
 				tc.inputDate, tc.expectedYear, tc.expectedWeekNum, tc.expectedWeekday, year, weekNum, weekday)
 		}
