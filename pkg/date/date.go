4074765a4bba3bb820ada9caa4df5184cfec291b
anwy13 <anwy13@chinaunicom.cn>
2024-03-18 21:35:06 +0800

    date: update GetDateDetails

--- pkg/date/date.go
+++ pkg/date/date.go
@@ -32,10 +32,9 @@ func dateDetail2(date time.Time) (int, int, int) {
 
 // daysInMonth 返回给定时间的月份中的天数  
 func daysInMonth(t time.Time) int {  
-	// 获取下个月的第一天，然后减去一天，就得到了当前月份的最后一天  
-	nextMonth := t.AddDate(0, 1, 0)  
-	lastDayOfMonth := nextMonth.AddDate(0, 0, -1)  
-	return lastDayOfMonth.Day()  
+	// 设置日期为那个月的最后一天（通常是下个月的第0天）  
+	lastDay := time.Date(t.Year(), t.Month()+1, 0, 0, 0, 0, 0, t.Location())  
+	return lastDay.Day()   
 }  
 
 
@@ -65,28 +64,30 @@ func dateWeek(date time.Time) (int, int, time.Weekday) {
 	return year, week, weekDay
 }
 
-// TODO：2024-12-30的输出结果不太对
-func GetDateDetails(inputDate string) (int, int, time.Month, int, int, time.Weekday, error) {
+
+
+
+
+func GetDateDetails(inputDate string) (int, time.Month, int, int, time.Weekday, int, int, int, int, error) {
 	// 将输入的日期解析为time.Time类型
 	date, err := time.Parse("2006-01-02", inputDate)
 	if err != nil {
-		return 0, 0, 0, 0, 0, 0, fmt.Errorf("无效的日期格式: %s", err)
+		return 0, 0, 0, 0, 0, 0, 0, 0,0, fmt.Errorf("无效的日期格式: %s", err)
 	}
 
-	// 获取年份、月份、周数和星期几
 	year := date.Year()
-	dayOfYear := dayOfYear(date)
 	month := date.Month()
-	dayOfMonth := date.Day()
-	_, weekNumber := date.ISOWeek()
-	weekday := date.Weekday()
 	
+	weekyear, week := date.ISOWeek()
+	weekday := date.Weekday()  // 星期几
 
-	// 如果日期在第一周，但是年份不同，需要修正周数
-	if weekNumber == 1 && date.Month() == 12 {
-		year++
-	}
+	dayOfMonth := date.Day()
+	dayOfYear := dayOfYear(date) 
 
-	return year, dayOfYear, month, dayOfMonth, weekNumber, weekday, nil
+	daysInMonth := daysInMonth(date)
+	daysInYear := daysInYear(date)
+	
+	
+	return year, month, weekyear, week, weekday, dayOfMonth, dayOfYear, daysInMonth, daysInYear, nil
 }
 
--- pkg/date/date_test.go
+++ pkg/date/date_test.go
@@ -44,6 +44,8 @@ func TestDaysInMonth(t *testing.T) {
 	} {
 		{"2023-01-01", 31},
 		{"2023-02-01", 28},
+		{"2023-02-05", 28},
+		{"2023-02-28", 28},
 		{"2023-03-01", 31},
 		{"2023-04-01", 30},
 		{"2023-05-01", 31},
@@ -55,6 +57,9 @@ func TestDaysInMonth(t *testing.T) {
 		{"2023-11-01", 30},
 		{"2023-12-01", 31},
 		{"2024-01-01", 31},
+		{"2024-01-02", 31},
+		{"2024-01-03", 31},
+		{"2024-01-31", 31},
 		{"2024-02-01", 29},
 	}
 
@@ -218,92 +223,43 @@ func TestGetDateDetails(t *testing.T) {
 	testCases := []struct {
 		inputDate        string
 		expectedYear     int
-		expectedDayOfYear int
-		expectedMonth    time.Month
+		expectMonth      time.Month
+		expectedWeekYear int
+		expectedWeek      int
+		expectedWeekday   time.Weekday
 		expectedDayOfMonth int
-		expectedWeekNum  int
-		expectedWeekday  time.Weekday
-		expectedErrorMsg string
+		expectedDayOfYear int
+		expectedDaysInMonth int
+		expectedDaysInYear int
 	}{
-		// // 2022年年初8天
-		// {"2022-01-01", 2021, 52, time.Saturday, ""},   // 周六
-		// {"2022-01-02", 2022, 1, time.Sunday, ""},     // 周日
-		// {"2022-01-03", 2022, 1, time.Monday, ""},     // 周一
-		// {"2022-01-04", 2022, 1, time.Tuesday, ""},    // 周二
-		// {"2022-01-05", 2022, 1, time.Wednesday, ""},  // 周三
-		// {"2022-01-06", 2022, 1, time.Thursday, ""},   // 周四
-		// {"2022-01-07", 2022, 1, time.Friday, ""},     // 周五
-		// {"2022-01-08", 2022, 1, time.Saturday, ""},   // 周六
-		// // 2022年年末8天
-		// {"2022-12-25", 2022, 51, time.Sunday, ""},    // 周日
-		// {"2022-12-26", 2022, 52, time.Monday, ""},    // 周一
-		// {"2022-12-27", 2022, 52, time.Tuesday, ""},   // 周二
-		// {"2022-12-28", 2022, 52, time.Wednesday, ""}, // 周三
-		// {"2022-12-29", 2022, 52, time.Thursday, ""},  // 周四
-		// {"2022-12-30", 2022, 52, time.Friday, ""},    // 周五
-		// {"2022-12-31", 2022, 52, time.Saturday, ""},  // 周六
-		// {"2023-01-01", 2023, 1, time.Sunday, ""},     // 周日
-		// // 2023年年初8天
-		// {"2023-01-01", 2023, 1, time.Monday, ""},     // 周一
-		// {"2023-01-02", 2023, 1, time.Tuesday, ""},    // 周二
-		// {"2023-01-03", 2023, 1, time.Wednesday, ""},  // 周三
-		// {"2023-01-04", 2023, 1, time.Thursday, ""},   // 周四
-		// {"2023-01-05", 2023, 1, time.Friday, ""},     // 周五
-		// {"2023-01-06", 2023, 1, time.Saturday, ""},   // 周六
-		// {"2023-01-07", 2023, 1, time.Sunday, ""},     // 周日
-		// {"2023-01-08", 2023, 2, time.Monday, ""},     // 周一
-		// // 2023年年末8天
-		// {"2023-12-25", 2023, 52, time.Monday, ""},    // 周一
-		// {"2023-12-26", 2023, 52, time.Tuesday, ""},   // 周二
-		// {"2023-12-27", 2023, 52, time.Wednesday, ""}, // 周三
-		// {"2023-12-28", 2023, 52, time.Thursday, ""},  // 周四
-		// {"2023-12-29", 2023, 52, time.Friday, ""},    // 周五
-		// {"2023-12-30", 2023, 52, time.Saturday, ""},  // 周六
-		// {"2023-12-31", 2023, 52, time.Sunday, ""},    // 周日
-		// 2024年年初8天
-		{"2024-01-01", 2024, 1, 1, 1, 1, time.Monday, ""},     // 周一
-		{"2024-01-02", 2024, 2, 1, 2, 1, time.Tuesday, ""},
-		{"2024-01-03", 2024, 3, 1, 3, 1, time.Wednesday, ""},
-		{"2024-01-04", 2024, 4, 1, 4, 1, time.Thursday, ""},
-		{"2024-01-05", 2024, 5, 1, 5, 1, time.Friday, ""},
-		{"2024-01-06", 2024, 6, 1, 6, 1, time.Saturday, ""},
-		{"2024-01-07", 2024, 7, 1, 7, 1, time.Sunday, ""},
-		{"2024-01-08", 2024, 8, 1, 8, 2, time.Monday, ""},	
-		// 2024年年末8天
-		// {"2024-12-25", 2024, 12, 25, 52, time.Wednesday, ""},
-		// {"2024-12-26", 2024, 12, 26, 52, time.Thursday, ""},
-		// {"2024-12-27", 2024, 12, 27, 52, time.Friday, ""},
-		// {"2024-12-28", 2024, 12, 28, 52, time.Saturday, ""},
-		// {"2024-12-29", 2024, 12, 29, 52, time.Sunday, ""},
-		// {"2024-12-30", 2024, 53, time.Monday, ""},
-		// {"2024-12-31", 2024, 53, time.Tuesday, ""},
-
+		{"2021-01-01", 2021, 1, 2020, 53, time.Friday, 1, 1, 31, 365},
+		{"2021-01-02", 2021, 1, 2020, 53, time.Saturday, 2, 2, 31, 365},
+		{"2021-01-03", 2021, 1, 2020, 53, time.Sunday, 3, 3, 31, 365},
+		{"2021-01-04", 2021, 1, 2021, 1, time.Monday, 4, 4, 31, 365},
+		{"2021-01-05", 2021, 1, 2021, 1, time.Tuesday, 5, 5, 31, 365},
+		{"2021-12-27", 2021, 12, 2021, 52, time.Monday, 27, 361, 31, 365},
+		{"2021-12-28", 2021, 12, 2021, 52, time.Tuesday, 28, 362, 31, 365},
+		{"2024-01-01", 2024, 1, 2024, 1, time.Monday, 1, 1, 31, 366},
+		{"2024-01-02", 2024, 1, 2024, 1, time.Tuesday, 2, 2, 31, 366},
+		{"2024-01-03", 2024, 1, 2024, 1, time.Wednesday, 3, 3, 31, 366},
+		{"2024-01-04", 2024, 1, 2024, 1, time.Thursday, 4, 4, 31, 366},
+		{"2024-01-05", 2024, 1, 2024, 1, time.Friday, 5, 5, 31, 366},
+		{"2024-01-06", 2024, 1, 2024, 1, time.Saturday, 6, 6, 31, 366},
+		{"2024-01-07", 2024, 1, 2024, 1, time.Sunday, 7, 7, 31, 366},
+		{"2024-01-08", 2024, 1, 2024, 2, time.Monday, 8, 8, 31, 366},
+		{"2024-02-01", 2024, 2, 2024, 5, time.Thursday, 1, 32, 29, 366},
+		
 	}
 
 	// 遍历测试用例表
 	for _, tc := range testCases {
-		year, dayOfYear, month, dayOfMonth, weekNum, weekday, err := GetDateDetails(tc.inputDate)
-
-		// 检查错误情况
-		if tc.expectedErrorMsg != "" {
-			if err == nil || err.Error() != tc.expectedErrorMsg {
-				t.Errorf("对于输入日期 %s，期望错误: %s，实际错误：%v", tc.inputDate, tc.expectedErrorMsg, err)
-			}
-			continue
-		} else {
-			if err != nil {
-				t.Errorf("对于输入日期 %s，不应有错误: %v", tc.inputDate, err)
-				continue
-			}
+		year, month, weekYear, week, weekday, dayOfMonth, dayOfYear, daysInMonth, daysInYear, err := GetDateDetails(tc.inputDate)
+		if err != nil {
+			t.Errorf("getDateDetails(%s) returned error: %v", tc.inputDate, err)
 		}
-
-		// 检查结果
-		if year != tc.expectedYear || dayOfYear != tc.expectedDayOfYear || month != tc.expectedMonth || dayOfMonth != tc.expectedDayOfMonth || weekNum != tc.expectedWeekNum || weekday != tc.expectedWeekday {
-			t.Errorf("对于输入日期 %s，期望的结果为 年份：%d，日数：%d，月份：%d，日：%d，周数：%d，星期几：%s，实际结果为 年份：%d，日数：%d，月份：%d，日：%d，周数：%d，星期几：%s",
-				tc.inputDate, tc.expectedYear, tc.expectedDayOfYear, tc.expectedMonth, tc.expectedDayOfMonth, tc.expectedWeekNum, tc.expectedWeekday, year, dayOfYear, month, dayOfMonth, weekNum, weekday)
-			// t.Errorf("对于输入日期 %s，期望的结果为 年份：%d，周数：%d，星期几：%s，实际结果为 年份：%d，周数：%d，星期几：%s",
-				// tc.inputDate, tc.expectedYear, tc.expectedWeekNum, tc.expectedWeekday, year, weekNum, weekday)
-		}
+		if year != tc.expectedYear || month != tc.expectMonth || weekYear != tc.expectedWeekYear || week != tc.expectedWeek || weekday != tc.expectedWeekday || dayOfMonth != tc.expectedDayOfMonth || dayOfYear != tc.expectedDayOfYear || daysInMonth != tc.expectedDaysInMonth || daysInYear != tc.expectedDaysInYear {
+				t.Errorf("getDateDetails(%s) returned (%d, %d, %d, %d, %s, %d, %d, %d, %d), expected (%d, %d, %d, %d, %s, %d, %d, %d, %d)", tc.inputDate, year, month, weekYear, week, weekday, dayOfMonth, dayOfYear, daysInMonth, daysInYear, tc.expectedYear, tc.expectMonth, tc.expectedWeekYear, tc.expectedWeek, tc.expectedWeekday, tc.expectedDayOfMonth, tc.expectedDayOfYear, tc.expectedDaysInMonth, tc.expectedDaysInYear)
+			}
 	}
 }
 
