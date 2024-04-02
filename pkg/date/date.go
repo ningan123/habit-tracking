d02a2a91d2373576c276a30d5b6263dfbd7df1b9
anwy13 <anwy13@chinaunicom.cn>
2024-04-02 16:01:56 +0800

    date: add yearNum info

--- pkg/date/date.go
+++ pkg/date/date.go
@@ -9,9 +9,9 @@ func convertDateStrToDate(dateStr string) (time.Time, error) {
 	return time.Parse("2006-01-02", dateStr)
 }
 
-func convertDateToDateStr(date time.Time) string {
-	return date.Format("2006-01-02")
-}
+// func convertDateToDateStr(date time.Time) string {
+// 	return date.Format("2006-01-02")
+// }
 
 
 
@@ -23,10 +23,10 @@ func dateDetail(date time.Time) (int, time.Month, int) {
 	return year, month, day
 }
 
-func dateDetail2(date time.Time) (int, int, int) {
-	year, month, day := date.Date()
-	return year, int(month), day
-}
+// func dateDetail2(date time.Time) (int, int, int) {
+// 	year, month, day := date.Date()
+// 	return year, int(month), day
+// }
 
 
 
@@ -68,11 +68,11 @@ func dateWeek(date time.Time) (int, int, time.Weekday) {
 
 
 
-func GetDateDetails(inputDate string) (int, time.Month, string, int, int, string, string, int, int, int, int, error) {
+func GetDateDetails(inputDate string) (int, string, time.Month, string, int, int, string, string, int, int, int, int, error) {
 	// 将输入的日期解析为time.Time类型
 	date, err := time.Parse("2006-01-02", inputDate)
 	if err != nil {
-		return 0, 0, "", 0, 0, "", "", 0, 0, 0,0, fmt.Errorf("无效的日期格式: %s", err)
+		return 0, "", 0, "", 0, 0, "", "", 0, 0, 0,0, fmt.Errorf("无效的日期格式: %s", err)
 	}
 
 	weekday := WeekdayToChinese(date.Weekday())  // 星期几
@@ -83,6 +83,7 @@ func GetDateDetails(inputDate string) (int, time.Month, string, int, int, string
 	month := date.Month()
 	year := date.Year()
 	monthNum := fmt.Sprintf("%d-%02d", year, month)
+	yearNum := fmt.Sprintf("%d", year)
 	
 	dayOfMonth := date.Day()
 	dayOfYear := dayOfYear(date) 
@@ -90,7 +91,7 @@ func GetDateDetails(inputDate string) (int, time.Month, string, int, int, string
 	daysInMonth := daysInMonth(date)
 	daysInYear := daysInYear(date)
 
-	return year, month, monthNum, weekyear, week, weekday, weekNum, dayOfMonth, dayOfYear, daysInMonth, daysInYear, nil
+	return year, yearNum, month, monthNum, weekyear, week, weekday, weekNum, dayOfMonth, dayOfYear, daysInMonth, daysInYear, nil
 }
 
 // 根据Weekday类型返回中文星期几  
